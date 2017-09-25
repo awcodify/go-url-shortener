ad3eaa16a90e1d9bcd1049d4917a75b637deef89
Danu Akbar <awcodify@gmail.com>
2017-09-26 01:50:36 +0700

    add config option

--- main.go
+++ main.go
@@ -4,15 +4,24 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"os"
 	"strings"
 
+	"github.com/BurntSushi/toml"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
 )
 
 var codec Codec
+
+type Config struct {
+	DB_NAME     string
+	DB_USER     string
+	DB_PASSWORD string
+}
 
 type Url struct {
 	gorm.Model
@@ -63,9 +72,20 @@ func main() {
 }
 
 func Database() *gorm.DB {
-	db, err := gorm.Open("mysql", "username:password@/dbname?charset=utf8&parseTime=True&loc=Local")
+	var configfile = "./db.conf"
+	_, err := os.Stat(configfile)
 	if err != nil {
-		fmt.Errorf("failed to connect database")
+		log.Fatal("Config file is missing: ", configfile)
+	}
+
+	var config Config
+	if _, err := toml.DecodeFile(configfile, &config); err != nil {
+		log.Fatal(err)
+	}
+
+	db, err := gorm.Open("mysql", config.DB_USER+":"+config.DB_PASSWORD+"@/"+config.DB_NAME+"?charset=utf8&parseTime=True&loc=Local")
+	if err != nil {
+		panic("failed to connect database")
 	}
 	return db
 }
